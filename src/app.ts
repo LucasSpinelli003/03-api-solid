0a5484d93fd0256ab3bbb361b42943a586e800e5
LucasSpinelli003 <rm97757@fiap.com.br>
2024-02-28 14:55:04 -0300

    feat - config .env and test prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Lucas Spinelli",
+    email: "lucas@mail.com",
+  },
+});
