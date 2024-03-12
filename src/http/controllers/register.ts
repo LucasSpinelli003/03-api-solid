7a35f6430fc4ced5e41b478a7edc96f18f55b55f
LucasSpinelli003 <rm97757@fiap.com.br>
2024-03-12 18:10:13 -0300

    fix - add new path

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { FastifyRequest, FastifyReply } from "fastify";
-import { UserAlreadyExistsError } from "@/services/errors/invalid-credentials-error";
+import { UserAlreadyExistsError } from "@/services/errors/user-already-exists-error";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { RegisterService } from "@/services/register";
 
