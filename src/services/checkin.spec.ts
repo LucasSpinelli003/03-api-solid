c095449c51793b401dbde79e779de75506b2b0a6
LucasSpinelli003 <lucaszk003@gmail.com>
2024-03-30 21:46:43 -0300

    improve - make tests green with tdd

--- src/services/checkin.spec.ts
+++ src/services/checkin.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
-import { beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { CheckInService } from "./checkin";
 
 describe("Check in test", () => {
@@ -9,14 +9,24 @@ describe("Check in test", () => {
   beforeEach(() => {
     inMemoryCheckInRepository = new InMemoryCheckInsRepository();
     sut = new CheckInService(inMemoryCheckInRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it("should be able to create a new check-in", async () => {
-    const checkIn = await sut.execute({
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
       gymId: "testeGym",
       userId: "testeUser",
     });
-    expect(checkIn.checkIn.id).toEqual(expect.any(String));
+
+    console.log(checkIn.createdAt);
+    expect(checkIn.id).toEqual(expect.any(String));
   });
 
   it("should not be able to create two check-ins", async () => {
@@ -24,11 +34,11 @@ describe("Check in test", () => {
       gymId: "testeGym",
       userId: "testeUser",
     });
-    await expect(() => {
+    await expect(() =>
       sut.execute({
         gymId: "testeGym",
         userId: "testeUser",
-      });
-    }).rejects.toBeInstanceOf(Error);
+      }),
+    ).rejects.toBeInstanceOf(Error);
   });
 });
