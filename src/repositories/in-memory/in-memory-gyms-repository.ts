1ba8b3340d72e3021457808bac37219c2df67f5f
LucasSpinelli003 <lucaszk003@gmail.com>
2024-04-10 16:24:32 -0300

    [Feature] - fetch nearby service - implement fetch nearby service

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
 import { Gym, Prisma } from "@prisma/client";
-import { GymsRepository } from "../gyms-repository";
+import { FindManyNearby, GymsRepository } from "../gyms-repository";
 import { randomUUID } from "node:crypto";
+import { getDistanceBetweenCordinates } from "@/services/utils/get-distance-between-cordinates";
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
@@ -9,6 +10,19 @@ export class InMemoryGymsRepository implements GymsRepository {
     return this.items
       .filter((gym) => gym.title.includes(query))
       .slice((page - 1) * 20, page * 20);
+  }
+
+  async FindManyNearby(params: FindManyNearby) {
+    return this.items.filter((gym) => {
+      const distance = getDistanceBetweenCordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: gym.latitude.toNumber(),
+          longitude: gym.longitude.toNumber(),
+        },
+      );
+      return distance < 10;
+    });
   }
 
   async create(data: Prisma.GymCreateInput) {
