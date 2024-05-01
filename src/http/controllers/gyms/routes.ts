9d9b3ab73de07f608f65e864470232deebfdaba8
LucasSpinelli003 <lucaszk003@gmail.com>
2024-04-30 22:12:59 -0300

    [Feature] - routes - refactor name of routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
 import { create } from "./create";
 
-export async function appRoutes(app: FastifyInstance) {
+export async function appGymRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJwt);
 
   app.post("/gyms", create);
